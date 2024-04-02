820417e89f02feffc113db2ab9d2a1e12c9d527e
Anna Hodunko <fatova29@gmail.com>
2024-04-02 10:12:50 +0300

    problem solving

--- js/task-2.js
+++ js/task-2.js
@@ -4,9 +4,7 @@ function makeArray(firstArray, secondArray, maxLength) {
     const newArray = firstArray.concat(secondArray);
     if (maxLength < newArray.length) {
         return newArray.slice(0, maxLength);
-    } else {
-        return newArray;
-    }
+    } 
 }
 
 
--- js/task-3.js
+++ js/task-3.js
@@ -3,7 +3,7 @@
 function filterArray(numbers, value) {
     let newArray = [];
     for (let i = 0; i < numbers.length; i ++) {
-        if (numbers[i] > value); {
+        if (numbers[i] > value) {
             newArray.push(numbers[i]);
         }
     }
